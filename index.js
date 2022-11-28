c87749c27cacb16041eef7dbf76cb7f0df8db3ff
isaac <isaactonyloi@gmail.com>
2022-11-28 07:42:54 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(driversArray) {
+    let drivers = [...driversArray.slice(0,2)];
+    return drivers;
+
+}
+
+const returnLastTwoDrivers = function(driversArray) {
+    let lasttwo = [...driversArray.slice(-2)];
+    return lasttwo;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(fare) {
+        return fare * num;
+    }
+
+}
+
+function fareDoubler(fare) {
+    const doubler = createFareMultiplier(2)(fare);
+    return doubler;
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(driversArray, fnc) {
+    return fnc(driversArray);
+}
